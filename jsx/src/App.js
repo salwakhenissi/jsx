4f95d8a5d8cbd0c31b757b06dd84c91f1755c629
salwa khenissi <khenissi.salwa>
2022-04-13 13:21:11 +0000

    first commit

---
+++ jsx/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import './style.css';
+import Image from "./imageInSrc.jpg"
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red">Your name here </h1>
+          
+          <img className="image" src={Image} />
+          
+          <img src="/imageInPublic.jpg" />
+        </div>
+        <iframe width="320" height="240" src="https://www.youtube.com/embed/xpVfcZ0ZcFM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
